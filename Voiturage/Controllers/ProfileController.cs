df310256eaedd83c8c868eb985e219cd14b9d439
adrienphan <adrienphancaophai@gmail.com>
2022-02-14 10:17:07 +0100

    affichage des trajets créés par l'utilisateur

--- Voiturage/Controllers/ProfileController.cs
+++ Voiturage/Controllers/ProfileController.cs
@@ -15,18 +15,28 @@ namespace Voiturage.Controllers
         {
             _voiturageContext = db;
         }
-        public IActionResult ListTrips(int? id)
+        /**
+         * public IActionResult ListTrips(int? id: user id)
+         * 
+         */
+
+        public IActionResult ListTrips()
         {
+            byte[] userid = new byte[4];
+            bool connected = HttpContext.Session.TryGetValue("UserID", out userid);
+            if (connected)
+                ViewData["UserID"] = BitConverter.ToInt32(userid);
+            /*------------------------------*/
             ViewData["Title"] = "Vos trajets";
             var user = _voiturageContext.Utilisateurs;
-            Utilisateur currentUser = user.Find(id);
+            Utilisateur currentUser = user.Find(ViewData["UserID"]);
             IEnumerable<Trajet> trajets = _voiturageContext.Trajets.Include(t=> t.VilleArrivee).Include(t=>t.VilleDepart).Where(t => t.Passagers.Contains(currentUser) || t.Chauffeur == currentUser);
             ViewBag.trajets = trajets;
             
 
-            if(id == null || id == 0)
+            if(ViewData["UserID"] == null)
             {
-                return RedirectToAction("Home", "Index");
+                return RedirectToAction("Index", "Home");
             }
             
             return View(currentUser);
