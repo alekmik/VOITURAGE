c44f9a5e2cbf2796f0ff1bbe5ab3db97d1caa031
adrienphan <adrienphancaophai@gmail.com>
2022-02-15 15:20:58 +0100

    liste trajets + Avis

--- Voiturage/Controllers/ProfileController.cs
+++ Voiturage/Controllers/ProfileController.cs
@@ -25,20 +25,29 @@ namespace Voiturage.Controllers
             byte[] userid = new byte[4];
             bool connected = HttpContext.Session.TryGetValue("UserID", out userid);
             if (connected)
+            {
+                ViewData["User"] = _voiturageContext.Utilisateurs.FirstOrDefault(x => x.Id == BitConverter.ToInt32(userid));
                 ViewData["UserID"] = BitConverter.ToInt32(userid);
+            }
             /*------------------------------*/
             ViewData["Title"] = "Vos trajets";
-            var user = _voiturageContext.Utilisateurs;
-            Utilisateur currentUser = user.Find(ViewData["UserID"]);
-            IEnumerable<Trajet> trajets = _voiturageContext.Trajets.Include(t=> t.VilleArrivee).Include(t=>t.VilleDepart).Where(t => t.Passagers.Contains(currentUser) || t.Chauffeur == currentUser);
+            ViewData["Avis"] = _voiturageContext.Avis;
+            var users = _voiturageContext.Utilisateurs;
+            Utilisateur currentUser = users.Find(ViewData["UserID"]);
+            IEnumerable<Trajet> trajets = _voiturageContext.Trajets.Include(t=> t.VilleArrivee).Include(t=>t.VilleDepart).Include(t=>t.Avis).Where(t => t.Passagers.Contains(currentUser) || t.Chauffeur == currentUser).OrderBy(t => t.HeureDepart);
+            IEnumerable<Avis> avis = _voiturageContext.Avis.Include(a => a.Trajet).Include(a => a.UtilisateurNotant).Include(a => a.UtilisateurNote);
             ViewBag.trajets = trajets;
+            ViewBag.avis = avis;
             
 
             if(ViewData["UserID"] == null)
             {
                 return RedirectToAction("Index", "Home");
             }
-            
+
+            //var avis = _voiturageContext.Avis;
+            //ViewData["Avis"] = avis;
+
             return View(currentUser);
         }
     }
