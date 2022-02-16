ec5f30f2fcde5aa38688f9d651f0767988db2be7
hugo <hugo@aa55.fr>
2022-02-16 13:29:06 +0100

    avancement home page

---
+++ Voiturage/Models/SearchViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Voiturage.Models
+{
+	public class SearchViewModel
+	{
+
+		public int IdVilleDepart { get; set; }
+		public int IdVilleArrivee { get; set; }
+		public DateTime DateDepart { get; set; }
+		public int PlacesRequises { get; set; }
+	}
+}
+
