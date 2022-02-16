57a872f2933cc5065bd7b28a905a3cf53b58f194
hugo <hugo@aa55.fr>
2022-02-16 09:33:39 +0100

    home page

---
+++ Voiturage/Models/BestRides.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Voiturage.Models
+{
+	public class BestRides
+	{
+		public Ville ? VilleDepart { get; set; } 
+		public Ville ? VilleArrivee { get; set; } 
+		public double prixMini { get; set; }
+		public int nbTrajetPropose { get; set; }
+		
+	}
+}
+
